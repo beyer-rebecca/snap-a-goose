7d7b4dcf6ff9beb363c7cddba01f0c25971ffd18
Rebecca Beyer <beyerebe@hu-berlin.de>
2024-01-21 18:09:13 +0100

    doc: complete doc of util classes

--- app/src/main/java/birdgame/utils/Constants.java
+++ app/src/main/java/birdgame/utils/Constants.java
@@ -3,8 +3,20 @@ package birdgame.utils;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * The Constants class contains static nested classes for each level of the Bird Game.
+ * Each nested class defines the initial positions and boundaries of birds for that specific level.
+ */
 public class Constants {
+
+    /**
+     * Level1 class contains constants for the first level of the Bird Game.
+     */
     public class Level1 {
+        /**
+         * Static list of bird positions and boundaries for Level 1.
+         * Each Vec2 pair represents the initial position and boundary coordinates of a bird.
+         */
         public static List<Vec2> birdsPos = Arrays.asList(
                 new Vec2(new Vec2(221, 323), new Vec2(215, 266)), 
                 new Vec2(new Vec2(815, 370), new Vec2(815, 857)),
@@ -15,7 +27,13 @@ public class Constants {
                 );
     }
 
+    /**
+     * Level2 class contains constants for the second level of the Bird Game.
+     */
     public class Level2 {
+        /**
+         * Static list of bird positions and boundaries for Level 2.
+         */
         public static List<Vec2> birdsPos = Arrays.asList(
                 new Vec2(new Vec2(128, 374), new Vec2(122, 187)), 
                 new Vec2(new Vec2(176, 86), new Vec2(126, 226)),
@@ -26,14 +44,19 @@ public class Constants {
                 );
     }
     
+    /**
+     * Level3 class contains constants for the third level of the Bird Game.
+     */
     public class Level3 {
+        /**
+         * Static list of bird positions and boundaries for Level 3.
+         */
         public static List<Vec2> birdsPos = Arrays.asList(
                 new Vec2(new Vec2(70, 624), new Vec2(0, 81)), 
                 new Vec2(new Vec2(186, 393), new Vec2(177, 279)),
                 new Vec2(new Vec2(437, 92), new Vec2(427, 543)),
                 new Vec2(new Vec2(870, 429), new Vec2(860, 965)),
                 new Vec2(new Vec2(1148, 279), new Vec2(1136, 1280))
-                // new Vec2(new Vec2(562, 530), new Vec2(558, 585))
                 );
     }
 }
--- app/src/main/java/birdgame/utils/Vec2.java
+++ app/src/main/java/birdgame/utils/Vec2.java
@@ -1,12 +1,31 @@
 package birdgame.utils;
 
+/**
+ * The Vec2 class represents a 2-dimensional vector or a pair of coordinates in the Bird Game.
+ * This class is used to represent positions, dimensions, and other two-dimensional values within the game.
+ */
 public class Vec2 {
     public int x,y;
     public Vec2 left, right;
+
+    /**
+     * Constructs a Vec2 instance representing a point in 2D space.
+     *
+     * @param x The x-coordinate of the point.
+     * @param y The y-coordinate of the point.
+     */
     public Vec2(int x, int y){
         this.x = x;
         this.y = y;
     }
+
+     /**
+     * Constructs a Vec2 instance representing a pair of Vec2 points.
+     * This constructor is useful for defining bounds or ranges.
+     *
+     * @param left The Vec2 instance representing the left point.
+     * @param right The Vec2 instance representing the right point.
+     */
     public Vec2(Vec2 left, Vec2 right){
         this.left = left;
         this.right = right;
