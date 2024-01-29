0504050f9a0a1d1066a77c0cd60b1d67930cf4bf
Rebecca Beyer <beyerebe@hu-berlin.de>
2024-01-29 18:19:30 +0100

    doc:update JavaDoc

--- app/src/main/java/birdgame/utils/package-info.java
+++ app/src/main/java/birdgame/utils/package-info.java
@@ -4,7 +4,6 @@
  *
  * - {@link birdgame.utils.Vec2}: A class for representing 2D vectors or coordinate pairs, 
  *   used for positions and dimensions within the game.
- *
  * - {@link birdgame.utils.CButton}: A custom button component for interactive UI elements 
  *   in the game, capable of displaying text and images and responding to mouse events.
  *
